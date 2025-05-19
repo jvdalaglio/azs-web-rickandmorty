8811d68ed8431651249a7ebcdb9b6e8e7736f2c8
João Victor Dalaglio <jvdalaglio@gmail.com>
2025-05-19 09:49:50 -0300

    chore: adição das cores do projeto ao tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,28 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // ajuste conforme seu projeto
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "#4DF4E0", // Azul Turquesa
+        secondary: "#2D1A52", // Roxo escuro
+        accent: "#FFE348", // Amarelo destaque
+        background: "#0E0E2C", // Fundo escuro
+        surface: "#1C1C3A", // Cards e containers
+        text: "#FFFFFF", // Texto branco
+        statusAlive: "#00FF91", // Verde
+        statusDead: "#FF4E4E", // Vermelho
+        statusUnknown: "#CCCCCC", // Cinza
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+      screens: {
+        xs: "375px",
       },
     },
   },
   plugins: [],
 };
-export default config;
